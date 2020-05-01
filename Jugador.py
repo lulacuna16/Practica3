9ee42d6d79264aa818c5dd774c4cdbdbd57b5a05
Luis Cuevas <lulacuna16@gmail.com>
2020-04-30 22:02:55 -0500

    V12

--- Jugador.py
+++ Jugador.py
@@ -222,16 +222,16 @@ def jugar(matriz, TCPClientSocket,Jugadores,Cliente):
     print("Duracion de la partida %.2f segundos" %(final-inicio))
 
 
-#HOST = str(input("Ingrese IP del servidor: "))  # The server's hostname or IP address
-#PORT = int(input("Ingrese Puerto del servidor: "))  # The port used by the server
-HOST = "192.168.1.64" # Standard loopback interface address (localhost)
-PORT = 56432  # Port to listen on (non-privileged ports are > 1023)
+HOST = str(input("Ingrese IP del servidor: "))  # The server's hostname or IP address
+PORT = int(input("Ingrese Puerto del servidor: "))  # The port used by the server
+#HOST = "192.168.1.64" # Standard loopback interface address (localhost)
+#PORT = 56432  # Port to listen on (non-privileged ports are > 1023)
 buffer_size = 1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPClientSocket:
     TCPClientSocket.connect((HOST, PORT))
 
-    Jugadores = int.from_bytes(TCPClientSocket.recv(int(buffer_size/2)), byteorder='little')  # Tener presente cuantos jugadores hay
+    Jugadores = int.from_bytes(TCPClientSocket.recv(int(buffer_size)), byteorder='little')  # Tener presente cuantos jugadores hay
     cliente = int.from_bytes(TCPClientSocket.recv(int(buffer_size/2)), 'little')  # Veririficar que jugador soy
     print("Jugador: ",cliente)
     if Jugadores==0:
